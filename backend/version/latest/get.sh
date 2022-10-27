b8971c09f68b84a4d4bf155d1bd66b2c1faa8226
tnan <mr.tnan@live.com>
2022-10-26 23:26:29 -0700

    Update get.sh

--- backend/version/latest/get.sh
+++ backend/version/latest/get.sh
@@ -1,2 +1,3 @@
-mkdir -p /root/seedbox/install/
 wget -O /root/seedbox/install/seedbox.zip https://bitbucket.org/mr-tnan/seedbox/downloads/seedbox-v1.0.zip
+wget -O /root/seedbox/install/install.sh https://mr-tnan.github.io/seedbox/backend/version/latest/install.sh
+bash /root/seedbox/install/install.sh 2>&1|tee /root/seedbox/install/install.log
